0976258e14d84dee9cf7571cb2c27f32a31b25c7
João Guilherme Cesar <74561539+Joao7747@users.noreply.github.com>
2021-03-08 21:28:38 -0300

    Add files via upload
    Arquivo final

---
+++ Conversa/src/conversa/Paquerador.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversa;
+
+/**
+ *
+ * @author Dell
+ */
+public class Paquerador extends Pessoa 
+{
+    public void cantada(String nome_pessoa){
+        
+        System.out.println(this.nome +": "+ nome_pessoa + ",asa de urubu, pena de galinha, se você quer um beijinho dê uma risadinha.");
+        
+    }
+    
+}
